315e4756035671a3fd391758a96b97b35d2ce832
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-07-29 00:05:50 +0200

    [add] edit name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-export default function Player({ name, symbol, isActive }) {
-    useState();
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
@@ -8,18 +8,28 @@ export default function Player({ name, symbol, isActive }) {
         // questa funzione riceve come argomento il valore attuale dello stato e restituisce il nuovo valore dello stato
         setIsEditing((editing) => !editing);
 
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
-    let playerName = <span className="player-name">{name}</span>
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
     //let btnCaption = 'Edit';
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        );
         //let btnCaption = 'Save';
     }
     return (
         <li className={isActive ? 'active' : undefined}>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
 
                 <span className="player-symbol">{symbol}</span>
             </span>
